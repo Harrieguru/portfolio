34fccac8de3ab407021d534616a90cee45f176c4
unknown <Hartie102@gmail.com>
2022-11-01 01:11:06 -0400

    user authentication using passport final draft

---
+++ Desktop/portfolio/server/config/db.js
@@ -0,0 +1,5 @@
+module.exports = {
+  // URI: "mongodb://127.0.0.1:27017/contact_store",
+
+  URI: "mongodb+srv://user_404:NSe2vTy1y8KCLRro@cluster0.l0pfosq.mongodb.net/contact_store?retryWrites=true&w=majority",
+};
